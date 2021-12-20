0141bce0a932a693f6847ec909e80c34d02afc98
JisuEun <zkxktmxmfhvl@gmail.com>
2021-12-20 12:22:56 +0900

    rollback

--- app/src/main/java/com/example/cims/HCI_layer/Vaccine_UI/PfizerActivity.java
+++ app/src/main/java/com/example/cims/HCI_layer/Vaccine_UI/PfizerActivity.java
@@ -1,36 +1,154 @@
 package com.example.cims.HCI_layer.Vaccine_UI;
 
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+
+import com.example.cims.HCI_layer.MenuActivity;
+import com.example.cims.MainActivity;
+import com.example.cims.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import android.app.TabActivity;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TabHost;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.cims.R;
 
-public class PfizerActivity extends Fragment {
-    Context ct;
-    @Nullable
+public class PfizerActivity extends AppCompatActivity {
+    LinearLayout listView;
+    Button createTextView;
+    int i = 0;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_pfizer, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pfizer);
 
-        TextView text = (TextView)view.findViewById(R.id.listview);
-        Button createViewButton = (Button)view.findViewById(R.id.createTextView);
-        createViewButton.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.topbar_pfizer);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼 (왼쪽)
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24);
+
+
+        FloatingActionButton btn_home = findViewById(R.id.fab_home_pfizer);
+
+        btn_home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                text.setText(text.getText() + "hello\n");
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-        return view;
+
+        // 동적 생성 관련 코드(DB에서의 데이터 불러오기를 일단은 버튼으로 표현)
+        listView = findViewById(R.id.listView);
+        createTextView = findViewById(R.id.createTextView);
+        createTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createTextView();
+            }
+        });
+
+
+        // 화이자 Activity로 넘어갈 버튼
+        Button pfizerButton = (Button) findViewById(R.id.buttonPfizer);
+        pfizerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), PfizerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 모더나 Activity로 넘어갈 버튼
+        Button modernaButton = (Button) findViewById(R.id.buttonModerna);
+        modernaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ModernaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        // 얀센 Activity로 넘어갈 버튼
+        Button janssenButton = (Button) findViewById(R.id.buttonJanssen);
+        janssenButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), JanssenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+        // 아스트라제네카 Activity로 넘어갈 버튼
+        Button astraZenecaButton = (Button) findViewById(R.id.buttonAstraZeneca);
+        astraZenecaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AstraZenecaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+    private void createTextView() {
+        TextView textViewNm = new TextView(this);
+        textViewNm.getText();
+        if (++i < 10) textViewNm.setText(i + "                     두통 및 현기증을 느낀다.");
+        else textViewNm.setText(i + "                   두통 및 현기증을 느낀다.");
+        textViewNm.setTextSize(12);
+        textViewNm.setTextColor(Color.BLACK);
+
+        textViewNm.setId(0);
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        param.gravity = Gravity.TOP;
+        textViewNm.setLayoutParams(param);
+        listView.addView(textViewNm);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+
     }
 }
--- app/src/main/java/com/example/cims/HCI_layer/Vaccine_UI/VaccineActivity.java
+++ app/src/main/java/com/example/cims/HCI_layer/Vaccine_UI/VaccineActivity.java
@@ -27,22 +27,10 @@ import androidx.viewpager.widget.ViewPager;
 public class VaccineActivity extends AppCompatActivity {
     int vac_id;
 
-    private ActivityVaccineBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_vaccine);
-
-        binding = ActivityVaccineBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        //ViewPager viewPager = binding.viewPager;
-        //viewPager.setAdapter(sectionsPagerAdapter);
-        //TabLayout tabs = binding.tabs;
-        //tabs.setupWithViewPager(viewPager);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.topbar_vaccine);
         toolbar.setTitle(R.string.app_name);
@@ -65,7 +53,61 @@ public class VaccineActivity extends AppCompatActivity {
             }
         });
 
+        // 화이자 Activity로 넘어갈 버튼
+        Button pfizerButton = (Button) findViewById(R.id.buttonPfizer);
+        pfizerButton.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                vac_id = 1;
+                Intent intent = new Intent(getApplicationContext(), PfizerActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // 모더나 Activity로 넘어갈 버튼
+        Button modernaButton = (Button) findViewById(R.id.buttonModerna);
+        modernaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                vac_id = 2;
+                Intent intent = new Intent(getApplicationContext(), ModernaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        // 얀센 Activity로 넘어갈 버튼
+        Button janssenButton = (Button) findViewById(R.id.buttonJanssen);
+        janssenButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                vac_id = 3;
+                Intent intent = new Intent(getApplicationContext(), JanssenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
+        // 아스트라제네카 Activity로 넘어갈 버튼
+        Button astraZenecaButton = (Button) findViewById(R.id.buttonAstraZeneca);
+        astraZenecaButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                vac_id = 4;
+                Intent intent = new Intent(getApplicationContext(), AstraZenecaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
